d2ea403e0781cdf1d2735be40ff160a78a17aa04
RomanZujev <roman.zujev@gmail.com>
2022-03-11 22:06:26 +0200

    added price element and classnames for CSS

--- src/components/OrderItem.js
+++ src/components/OrderItem.js
@@ -1,8 +1,9 @@
 export default function OrderItem(props) {
   return (
     <div className="orderItem">
-      <div>{props.title}</div>
-      <div>{props.amount}</div>
+      <div className="orderName">{props.title}</div>
+      <div className="orderAmount">{props.amount}</div>
+      <div className="orderPrice">{props.price}</div>
     </div>
   );
 }
