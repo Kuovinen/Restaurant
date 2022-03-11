6bc40f26778e1f11106b5401cefc4f6159bc91fc
RomanZujev <roman.zujev@gmail.com>
2022-03-11 22:03:22 +0200

    added state defined orderArray

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
+import React, { useState } from "react";
+
 import Header from "./components/Header.js";
 import Main from "./components/Main.js";
 
 function App() {
+  let [orderArray, setOrderArray] = useState([]);
+
   return (
     <div className="App">
       <Header />
-      <Main />
+      <Main orderArray={orderArray} setOrderArray={setOrderArray} />
     </div>
   );
 }
