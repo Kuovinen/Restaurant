35bcbc14bc4845ed17a8b986e0742df8467a8416
RomanZujev <roman.zujev@gmail.com>
2022-03-11 22:05:50 +0200

    added button handler function addToOrder

--- src/components/DishTab.js
+++ src/components/DishTab.js
@@ -1,12 +1,21 @@
 export default function DishTab(props) {
+  function addToOrder() {
+    let object = {
+      name: props.title,
+      amount: 1,
+      price: props.price,
+      key: props.id,
+    };
+    props.setOrderArray((original) => [...original, object]);
+  }
   return (
     <div className="dishTab">
       <img className="img" src={props.img} alt="" />
       <div className="titlePrice">
         <h4 className="title">{props.title}</h4>
-        <h4 className="price">{props.price}</h4>
+        <h4 className="price">{props.price}€</h4>
         <h5 className="calories">{props.calories}</h5>
-        <button className="add">
+        <button className="add" onClick={addToOrder}>
           <span>+</span>
           <span className="addTxt">ADD</span>
         </button>
@@ -14,4 +23,3 @@ export default function DishTab(props) {
     </div>
   );
 }
-//img={dish1} title="Double Avacado Toast" price="19.99$",
