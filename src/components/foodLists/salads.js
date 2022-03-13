34f56d2b76094f903ca5444f8ebb32130b35faba
RomanZujev <roman.zujev@gmail.com>
2022-03-13 15:37:51 +0200

    moved salads list to foodList folder

---
+++ src/components/foodLists/salads.js
@@ -0,0 +1,32 @@
+let dishesList = [
+  {
+    key: "3",
+    img: "/images/dish3.jpeg",
+    title: "Mozzarella in red",
+    calories: "300cal",
+    price: 10.99,
+  },
+  {
+    key: "4",
+    img: "/images/dish4.jpeg",
+    title: "Salad festo",
+    calories: "150cal",
+    price: 6.29,
+  },
+  {
+    key: "6",
+    img: "/images/dish6.jpeg",
+    title: "Rice salad",
+    calories: "300cal",
+    price: 16.99,
+  },
+  {
+    key: "7",
+    img: "/images/dish7.jpeg",
+    title: "Fruitti",
+    calories: "300cal",
+    price: 5.39,
+  },
+];
+
+export default dishesList;
