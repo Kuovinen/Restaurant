e35561c21d1a1e6ced585548c0ecff46b9fb45dd
RomanZujev <roman.zujev@gmail.com>
2022-04-04 17:11:32 +0300

    calorie data edits

--- src/components/foodLists/drinks.js
+++ src/components/foodLists/drinks.js
@@ -3,7 +3,7 @@ let dishesList = [
     key: "8",
     img: "/images/dish8.jpeg",
     title: "Coffee & Spice",
-    calories: "300cal",
+    calories: "190cal",
     price: 5.99,
     ingredients: "Black Coffee, Wheat Bun, Hazelnuts",
     warnings: { gluten: false, lactose: false, nuts: true },
--- src/components/foodLists/hots.js
+++ src/components/foodLists/hots.js
@@ -3,7 +3,7 @@ let dishesList = [
     key: "5",
     img: "/images/dish5.jpeg",
     title: "Ramen bistro",
-    calories: "300cal",
+    calories: "350cal",
     price: 15.99,
     ingredients: "Egg noodles, Onions, Paprika, Beef",
     warnings: { gluten: true, lactose: false, nuts: true },
