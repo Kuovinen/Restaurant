e65f9ab4aa54c478a620d8177eff1b720e96dc41
RomanZujev <roman.zujev@gmail.com>
2022-04-05 14:46:10 +0300

    implemented time and location selection

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,18 +1,58 @@
+import React from "react";
 export default function Header() {
+  const [hours, setHours] = React.useState("09");
+  const [minutes, setMinutes] = React.useState("00");
+  function chngHours(event) {
+    setHours(event.target.value);
+  }
+  function chngMins(event) {
+    setMinutes(event.target.value);
+  }
   return (
     <header>
-      <div className="logo">la CASA</div>
+      <div className="logo">
+        la<span style={{ color: "var(--cp-Green)" }}>C</span>ASA
+      </div>
 
       <div className="login">
         <button className="logBtn">SignIn</button>
       </div>
       <div className="locations">
         <span className="label">LOCATIONS:</span>
-        <div className="location">Keskivuori 78b, Helsinki</div>
+        <select class="locationSelect" name="option">
+          <option value="1">Keskivuori 78b, Helsinki</option>
+          <option value="2">Suomalahti 30, Vantaa</option>
+          <option value="3">Laarikoski 10a, Espoo</option>
+        </select>
       </div>
       <div className="time">
         <span className="label">DESIRED DELIVERY TIME: </span>{" "}
-        <div className="deliveryTime"></div>
+        <div
+          style={{
+            color: "var(--cp-White)",
+            margin: "0 1rem",
+            fontSize: "4vh",
+          }}
+        >
+          {hours}:{minutes}
+        </div>
+        <select className="deliveryTime" name="option" onChange={chngHours}>
+          <option value="09">09</option>
+          <option value="10">10</option>
+          <option value="11">11</option>
+          <option value="12">12</option>
+          <option value="13">13</option>
+          <option value="14">14</option>
+          <option value="15">15</option>
+          <option value="16">16</option>
+          <option value="17">17</option>
+        </select>
+        <select className="deliveryTime" name="option" onChange={chngMins}>
+          <option value="00">00</option>
+          <option value="15">15</option>
+          <option value="30">30</option>
+          <option value="45">45</option>
+        </select>
       </div>
     </header>
   );
