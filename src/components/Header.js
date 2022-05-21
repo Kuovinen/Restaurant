1858031478c2fc00a50f9d12b7bdd2eec4a05c91
RomanZujev <roman.zujev@gmail.com>
2022-05-21 19:19:54 +0300

    added extra widescreen suppor (mostly header with change implemented)

--- src/components/Header.js
+++ src/components/Header.js
@@ -13,48 +13,50 @@ export default function Header() {
   }
 
   return (
-    <header>
-      <div className="logo">
-        <span id="la">la</span>
-        <span style={{ color: "var(--cp-Green)" }}>C</span>asa
-      </div>
-      <LogInRegisterSection />
+    <div className="headContainer">
+      <header>
+        <div className="logo">
+          <span id="la">la</span>
+          <span style={{ color: "var(--cp-Green)" }}>C</span>asa
+        </div>
+        <LogInRegisterSection />
 
-      <div className="locations">
-        <span className="labelPlace">Locations:</span>
-        <select className="locationSelect" name="option">
-          <option value="1">Keskivuori 78b, Helsinki</option>
-          <option value="2">Suomalahti 30, Vantaa</option>
-          <option value="3">Laarikoski 10a, Espoo</option>
-        </select>
-      </div>
-      <div className="time">
-        <div>
-          <span className="labelTime">Delivery time: </span>
-          <span className="chosenTime">
-            {orderTime[0]}:{orderTime[1]}
-          </span>
-        </div>
-        <div>
-          <select className="deliveryTime" name="hours" onChange={modTime}>
-            <option value="09">09</option>
-            <option value="10">10</option>
-            <option value="11">11</option>
-            <option value="12">12</option>
-            <option value="13">13</option>
-            <option value="14">14</option>
-            <option value="15">15</option>
-            <option value="16">16</option>
-            <option value="17">17</option>
-          </select>
-          <select className="deliveryTime" name="minutes" onChange={modTime}>
-            <option value="00">00</option>
-            <option value="15">15</option>
-            <option value="30">30</option>
-            <option value="45">45</option>
+        <div className="locations">
+          <span className="labelPlace">Locations:</span>
+          <select className="locationSelect" name="option">
+            <option value="1">Keskivuori 78b, Helsinki</option>
+            <option value="2">Suomalahti 30, Vantaa</option>
+            <option value="3">Laarikoski 10a, Espoo</option>
           </select>
         </div>
-      </div>
-    </header>
+        <div className="time">
+          <div>
+            <span className="labelTime">Delivery time: </span>
+            <span className="chosenTime">
+              {orderTime[0]}:{orderTime[1]}
+            </span>
+          </div>
+          <div>
+            <select className="deliveryTime" name="hours" onChange={modTime}>
+              <option value="09">09</option>
+              <option value="10">10</option>
+              <option value="11">11</option>
+              <option value="12">12</option>
+              <option value="13">13</option>
+              <option value="14">14</option>
+              <option value="15">15</option>
+              <option value="16">16</option>
+              <option value="17">17</option>
+            </select>
+            <select className="deliveryTime" name="minutes" onChange={modTime}>
+              <option value="00">00</option>
+              <option value="15">15</option>
+              <option value="30">30</option>
+              <option value="45">45</option>
+            </select>
+          </div>
+        </div>
+      </header>
+    </div>
   );
 }
